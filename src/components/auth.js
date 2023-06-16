465c4468eba955c617e483c1a130bb79e3dc28c6
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-06-16 23:57:29 +0530

    created auth.js

---
+++ src/components/auth.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Auth() {
+  return (
+    <div>
+        <div className="inputs">
+            <h3>User Sign-Up</h3>
+
+            <input type="text" value={"Enter Email"}/>
+            <input type="password" value={"Enter Password"}/>
+
+        </div>
+    </div>
+  )
+}
+
+export default Auth
