5278c0cb8ccb2f2d66ac2e9d4bc517889fbe9e79
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-06-18 23:41:47 +0530

    signup UI

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import '../App.css'
 
 function SignUp() {
   return (
-    <div className='login'>
-        
+    <div className="signup">
+      <h2>Sign Up</h2>
+      <div className="sig-inputs">
+        <h3>Enter Your Details...</h3>
+        <input type="text" placeholder="Enter First Name" />
+        <input type="text" placeholder="Enter Last Name" />
+        <input type="email" placeholder="Enter Email" />
+        <input type="password" placeholder="Enter Password" />
+        <input type="password" placeholder="Confirm Password" />
+
+        <button className="sub-sig">Sign Up</button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default SignUp
