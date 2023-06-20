41a11a92b14c57e2743668d4bf0dee9f410b0d77
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-06-20 23:32:58 +0530

    auth

---
+++ src/components/land.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+
+function Land() {
+  return (
+    <div>
+        <Navbar>
+            
+        </Navbar>
+    </div>
+  )
+}
+
+export default Land
