98f1c1463e11fb88aa322dec3fcd898c906159a5
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-06-23 23:57:59 +0530

    signup api

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../App.css'
 
 function SignUp() {
+
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: ''
+  })
+
+  const handleFormSubm = (event) => {
+    event.preventDefault();
+
+    const data = JSON.stringify(formData);
+
+    fetch()
+
+  }
+
   return (
     <div className="signup">
       <h2>Sign Up</h2>
       <div className="sig-inputs">
         <h3>Enter Your Details...</h3>
-        <input type="text" placeholder="Enter First Name" />
-        <input type="text" placeholder="Enter Last Name" />
-        <input type="email" placeholder="Enter Email" />
-        <input type="password" placeholder="Enter Password" />
-        <input type="password" placeholder="Confirm Password" />
+        <form action="" method="post">
+          <input type="text" placeholder="Enter a Username" />
+          <input type="email" placeholder="Enter Email" />
+          <input type="password" placeholder="Enter Password" />
+          <input type="password" placeholder="Confirm Password" />
 
-        <button className="sub-sig">Sign Up</button>
+          <button className="sub-sig">Sign Up</button>
+
+        </form>
       </div>
     </div>
   );
