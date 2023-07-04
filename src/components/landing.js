8f8f42cc33ba60b12a882b7cce30a28ab465ea61
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-07-05 01:45:52 +0530

    before css

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,17 +1,45 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import SignUp from "./signup";
+import Login from "./login";
 
 function Landing() {
 	return (
 		<BrowserRouter>
-			<header></header>
+			<header>
+				<nav className="navbar">
+					<div className="land-nav-a">
+						<Link to="/">Home</Link>
+						<Link to="/product">Product</Link>
+						<Link to="/pricing">Pricing</Link>
+					</div>
+
+					<div className="land-nav-b">
+						<Link to="/register">Register</Link>
+						<Link to="/login">Sign-In</Link>
+					</div>
+				</nav>
+			</header>
 
 			<div className="landing">
-				<nav></nav>
-				<div className="first-content"></div>
-				<div className=""></div>
+				<div className="first-content">
+					{/* animated heading that appears letter by letter */}
+					<h1>Welcome to the doorway of Future!</h1>
+					<p></p>
+				</div>
+				<div className="buttons-div">
+					<h3>Get on board: </h3>
+					<button>Register</button>
+					<button>Sign-In</button>
+				</div>
 			</div>
-			<main></main>
+			<main>
+				<Routes>
+					<Route path="/" element={<Landing />} />
+					<Route path="/register" element={<SignUp />} />
+					<Route path="/login" element={<Login />} />
+				</Routes>
+			</main>
 		</BrowserRouter>
 	);
 }
