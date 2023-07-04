4923f203535fb99673ba5312dfd25c21738b8b6a
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-07-04 23:50:31 +0530

    landing redefined

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import './App.css';
-import SignUp from './components/signup';
-import Login from './components/login'
-
-
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import SignUp from "./components/signup";
+import Login from "./components/login";
+import Landing from "./components/landing";
 
 function App() {
-  return (
-    <div className="App">
-      <SignUp></SignUp>
-      {/* <Login></Login> */}
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<header></header>
+
+			<div className="App">
+				<SignUp></SignUp>
+				{/* <Login></Login> */}
+			</div>
+			<main>
+				<Routes>
+					<Route path="/" element={<Landing />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<SignUp />} />
+				</Routes>
+			</main>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/components/landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Landing() {
+	return (
+		<div className="landing">
+			<div></div>
+			<div></div>
+		</div>
+	);
+}
+
+export default Landing;
