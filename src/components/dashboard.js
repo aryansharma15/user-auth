0180d28abbb558fbd94f6885a3fc48549dc4212f
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-07-18 23:12:26 +0530

    dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -3,7 +3,13 @@ import React from "react";
 function Dashboard() {
 	return (
 		<div>
-			<div></div>
+			<div>
+				<h1>Welcome "User"</h1>
+				<p>
+					Check lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam perferendis culpa vero fuga eaque et magnam quaerat numquam
+					ipsum quis?
+				</p>
+			</div>
 		</div>
 	);
 }
